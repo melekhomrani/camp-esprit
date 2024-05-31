dffd882f289a3a5d3afb10b54a4766c8f9c14035
melekhomrani <melekhomrani87@gmail.com>
2024-05-31 15:45:47 +0100

    initial commit

---
+++ src/main/java/tn/esprit/campesprit/CampEspritApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.campesprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampEspritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampEspritApplication.class, args);
+	}
+
+}
