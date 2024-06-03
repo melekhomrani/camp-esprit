207623c25aaca6b5295613ecd2be19c0380b7d92
melekhomrani <melekhomrani87@gmail.com>
2024-06-03 16:04:47 +0100

    chore: Add HttpTokenInterceptor for Keycloak integration

--- camp-esprit-client/src/app/app-routing.module.ts
+++ camp-esprit-client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './services/guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
